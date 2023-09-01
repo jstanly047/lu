17b3b18df58aec130db8d0ceef89a82d7eb44a84
jstanly <jstanly047@gmail.com>
2023-09-01 19:35:23 +0800

    Migrating build to conan

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 import os
 from conan import ConanFile
 from conan.tools.files import copy
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class snapafwRecipe(ConanFile):
     name = "lu_platform"
@@ -13,25 +14,68 @@ class snapafwRecipe(ConanFile):
     url = ""
     description = "Application framework for Lu"
 
+    settings ="build_type"
+
     options = {
     "shared": [True, False],
-    "fPIC": [True, False],
+    "UNIT_TEST": [True, False],
+    "COV_BUILD": [True, False]
     }
 
     default_options = {
-    "fPIC": False,
     "shared": False,
+    "UNIT_TEST": False,
+    "COV_BUILD": False
     }
 
+    def config_options(self):
+        if self.settings.build_type == "Release":
+            self.options.COV_BUILD = False
+        elif self.settings.build_type == "Coverage":
+            self.options.UNIT_TEST = True
+            self.options.COV_BUILD = True
+
+    def configure(self):
+        pass
+
     def layout(self):
-        self.cpp.source.includedirs = ["include"]
-        self.cpp.build.libdirs = ["dist/lib"]
+        cmake_layout(self)
+        #self.cpp.source.includedirs = ["include"]
+        #self.cpp.build.libdirs = ["dist/lib"]
+        #self.cpp.build.bindirs = ["dist/bin"]
+
+    def requirements(self):
+        if self.options.UNIT_TEST:
+            self.test_requires("gtest/1.14.0")
+    
+    def generate(self):
+        tc = CMakeToolchain(self)
+        if self.options.UNIT_TEST:
+            tc.variables["UNIT_TEST"] = True
+        if self.options.COV_BUILD:
+            tc.variables["COV_BUILD"] = True
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        #if not self.conf.get("tools.build:skip_test", default=False):
+        #    self.run(os.path.join(self.cpp.build.bindirs[0], "LuTests"))
 
     def package(self):
-        local_include_folder = os.path.join(self.source_folder, self.cpp.source.includedirs[0])
-        local_lib_folder = os.path.join(self.build_folder, self.cpp.build.libdirs[0])
-        copy(self, "*.h", local_include_folder, os.path.join(self.package_folder, "include"), keep_path=True)
-        copy(self, "*.a", local_lib_folder, os.path.join(self.package_folder, "lib"), keep_path=True)
+        print("sdfsdf")
+        copy(self, "*.h", self.cpp.source.includedirs[0], os.path.join(self.package_folder, "include"), keep_path=True)
+        copy(self, "*.a", self.cpp.build.libdirs[0], os.path.join(self.package_folder, "lib"), keep_path=True)
 
     def package_info(self):
         self.cpp_info.libs = ["lu_platform"]
+        self.cpp_info.libdirs = ["lib"]
+        self.cpp_info.includedirs = ["include"]
+
+        '''
+        if self.options.shared:
+            self.cpp_info.libs = ["LuTests"]
+        else:
+            self.cpp_info.libs = ["LuTests"]
+        '''
